0cbfb08578853431bbd10e071030a23bc419bd89
andres mogollon <andresmogollob@gmail.com>
2022-05-27 12:29:19 -0500

    first commit

---
+++ Contactame/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from .forms import Formulario_contacto
+# from django.core.mail import EmailMessage
+from django.core.mail import BadHeaderError, send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def contactame(request):
+    if (request.method == "POST"):
+        formulario = Formulario_contacto(data = request.POST)
+        if (formulario.is_valid()):
+            nombre = request.POST.get('nombre') 
+            email = request.POST.get('email') 
+            contenido = request.POST.get('contenido') 
+            mensaje = 'Hola soy {}, mi correo es {} me comunico para decirte que: \n\n{}'.format(nombre,email,contenido)
+
+            # email = EmailMessage('PERSONAL WEBSITE ANDRES:', 
+            # 'Hola soy {}, mi correo es {} me comunico para decirte que: \n\n{}'.format(nombre,email,contenido), 
+            # '', [settings.EMAIL_HOST_USER], reply_to=[email])
+
+            # try:
+            #     email.send()
+            #     return redirect('/contactame/?valido')
+            # except:
+            #     return redirect('/contactame/?novalido')
+            try:
+                send_mail(nombre, mensaje, 'andresmogollob@gmail.com', ['andresmogollob@gmail.com','erikadayanatobi@gmail.com'])
+            except BadHeaderError:
+                return redirect('/contactame/?novalido')
+            return redirect('/contactame/?valido')
+
+    else:
+        formulario = Formulario_contacto()
+
+    return render(request, "Contactame/contactame.html",{'formulario':formulario})
---
+++ Proyectos/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+from Proyectos.models import proyectos, categorias, tecnologias
+from django.db.models import Q
+# Create your views here.
+
+
+
+def cantidad():
+    categoris = []
+    tecnologis = []
+    categoriss = categorias.objects.all()
+    tecnologiss = tecnologias.objects.all()
+    proyects = proyectos.objects.all()
+
+
+    for cate in categoriss:
+        cc = 0
+        for proyec in proyects:
+            if cate == proyec.categoria:
+                cc += 1
+        categoris.append({'id':cate.id,
+                        'nombre':cate.nombre,
+                        'cantidad':cc})
+
+    for tecnolo in tecnologiss:
+        cc = 0
+        for proyec in proyects:
+            for proy in proyec.tecnologia.all():
+                if tecnolo == proy:
+                    cc += 1
+        tecnologis.append({'id':tecnolo.id,
+                        'nombre':tecnolo.nombre,
+                        'cantidad':cc})
+    return categoris, tecnologis
+
+def buscar(queryset):
+    mensaje = False
+    proyects = proyectos.objects.filter(
+        Q(nombre__icontains = queryset) |
+        Q(categoria__nombre__icontains = queryset) |
+        Q(tecnologia__nombre__icontains = queryset) 
+    ).distinct()
+    if (queryset == 'todos') | (queryset == 'todo'):
+        proyects = proyectos.objects.all()
+    elif not proyects:
+        mensaje = queryset
+    return mensaje, proyects
+
+def proyectoss(request):
+    
+    categoris,tecnologis = cantidad()
+    proyects = proyectos.objects.all()
+    proyects = proyects
+    queryset = request.POST.get("buscar")
+    mensaje = False
+    if queryset:
+        mensaje, proyects = buscar(queryset)
+    return render(request,'Proyectos/proyectos.html',{'proyects': proyects, 
+                                                        "categoris":categoris, 
+                                                        "tecnologis":tecnologis,
+                                                        "mensaje":mensaje})
+
+def categoria(request, categoria_id):
+    categoris,tecnologis = cantidad()
+    categori = categorias.objects.get(id = categoria_id)
+    proyects = proyectos.objects.filter(categoria = categoria_id)
+    queryset = request.POST.get("buscar")
+    mensaje = False
+    if queryset:
+        mensaje, proyects = buscar(queryset)
+    return render(request, 'Proyectos/categoria.html', {"categori":categori, 
+                                                        "categoris":categoris,
+                                                        "proyects":proyects, 
+                                                        "tecnologis":tecnologis,
+                                                        "mensaje":mensaje})
+
+def tecnologia(request, tecnologia_id):
+    categoris,tecnologis = cantidad()
+    tecnologi = tecnologias.objects.get(id = tecnologia_id)
+    proyects = proyectos.objects.filter(tecnologia = tecnologia_id)
+    queryset = request.POST.get("buscar")
+    mensaje = False
+    if queryset:
+        mensaje, proyects = buscar(queryset)
+    return render(request, 'Proyectos/tecnologia.html', {"tecnologi":tecnologi, 
+                                                        "categoris":categoris,
+                                                        "proyects":proyects, 
+                                                        "tecnologis":tecnologis,
+                                                        "mensaje":mensaje})
+
