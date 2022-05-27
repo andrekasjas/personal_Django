0fd6eea9aa3a43ee82609a68b18044234c3862de
Andres Mogollon <62920120+andrekasjas@users.noreply.github.com>
2022-05-27 13:02:33 -0500

    Update settings.py

--- hola/settings.py
+++ hola/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^82p$dk5pp9s5*qws*p2)^%$9=((kfdfmg#5ciii6k_ufdma^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
