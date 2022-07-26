53bacb5547ba9acab3f4065c130e4736087cb1ea
Andres Mogollon <62920120+andrekasjas@users.noreply.github.com>
2022-07-26 12:55:52 -0500

    Update views.py

--- Home/views.py
+++ Home/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'Home/home.html')
+    user = User.objects.last()
+    url = "https://res.cloudinary.com/dc1hb2uev/image/upload/fl_attachment/{}/personal/yo/CV_Andres_Mogollon.pdf".format(user.first_name)
+    return render(request, 'Home/home.html',{"url":url})
